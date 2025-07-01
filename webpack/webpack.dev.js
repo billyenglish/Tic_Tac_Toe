8dc565d21e7fd1cf06e94c0bdbb3a091b7c72465
Demar English <demarenglish@Mac.attlocal.net>
2025-07-01 17:40:13 -0400

    Updated the Webpack Production file.

--- webpack/webpack.dev.js
+++ webpack/webpack.dev.js
@@ -1,12 +1,11 @@
 const { merge } = require('webpack-merge');
-const common = require('./webpack.common.js');
+const common = require('./webpack.config.js');
 
 module.exports = merge(common, {
     mode: 'development',
     devtool: 'inline-source-map',
     devServer: {
         static: './dist',
-        port: 3000,
         open: true,
     },
 });
